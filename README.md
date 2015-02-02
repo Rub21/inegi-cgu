7c2601dff59d80638b18a73dfa685bb5516c58f2
Rub21 <ruben@mapbox.com>
2015-02-01 22:29:04 -0500

    archivo de descarga

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # inegi-cgu
+
+##### Download  all files from :
+
+http://www3.inegi.org.mx/sistemas/productos/default.aspx?c=265&upc=0&s=est&tg=3604&f=2&cl=0&pf=prod&ef=0&ct=201100000&pg=2
+
+###### Run:
+
+`node download.js`
+
+**Numero de archivos : ** 2456
--- download.js
+++ download.js
@@ -16,6 +16,7 @@ fs.readFile('inegi.json', 'utf8', downloading);
 
 function downloading(err, data) {
 	var json = JSON.parse(data);
+	console.log(json.length);
 	for (var i = 2; i < 4; i++) {
 		console.log(json[i].entidad);
 		mkdirSync(path.join(json[i].entidad));
