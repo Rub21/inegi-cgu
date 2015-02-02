c675e618de803225bcee86302acf060a8a162506
Rub21 <ruben@mapbox.com>
2015-02-01 22:46:21 -0500

    not directory

--- download.js
+++ download.js
@@ -12,11 +12,10 @@ var open = require('open');
 
 //read file
 fs.readFile('inegi.json', 'utf8', downloading);
-
 function downloading(err, data) {
 	var json = JSON.parse(data);
-	console.log(json.length);
-	for (var i = 2; i < 4; i++) {
+	console.log('Files to download : ' + json.length);
+	for (var i = 0; i < json.length; i++) {
 		console.log(json[i].entidad);
 		mkdirSync(path.join(json[i].entidad));
 		open(json[i].url);
